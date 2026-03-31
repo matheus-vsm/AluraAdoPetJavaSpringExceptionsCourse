d21dd43634fcbb7c98c75117c57f15658895ea0d
matheus-vsm <scompinha@hotmail.com>
2026-03-31 19:56:03 -0300

    aula 1.5

--- src/main/java/PilhaDeExecucao.java
+++ src/main/java/PilhaDeExecucao.java
@@ -10,16 +10,27 @@ public class PilhaDeExecucao {
     public static void metodo2() {
         System.out.println("[Inicio] - metodo2");
 
-        Usuario usuario = new Usuario("Vinicius");
-        System.out.println(usuario.nome);
+//        Usuario usuario = new Usuario("Vinicius")
+//        System.out.println(usuario.nome);
+        Usuario usuario = null;
+
+        try {
+            System.out.println(usuario.nome);
+        } catch (NullPointerException e) {
+            System.out.println("Usuário não encontrado! Metodo2");
+        }
 
         System.out.println("[Fim] - metodo2");
     }
 
     public static void main(String[] args) {
         System.out.println("[Inicio] - main");
-
-        metodo1();
+//        metodo1();
+        try { // ambos funcionam. tirar o try catch de cima para esse funcionar.
+            metodo1();
+        } catch (NullPointerException e) {
+            System.out.println("Usuário não encontrado! Main");
+        }
 
         System.out.println("[Fim] - main");
     }
@@ -27,6 +38,7 @@ public class PilhaDeExecucao {
 
 class Usuario {
     public String nome;
+
     public Usuario(String nome) {
         this.nome = nome;
     }
